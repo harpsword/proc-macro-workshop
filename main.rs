93d739bef2eba0cd1aa39c3705a22bf183e428f6
harpsword <harpswordyyl@gmail.com>
2021-11-28 15:39:28 +0800

    finish builder to 06 test

--- main.rs
+++ main.rs
@@ -1,9 +1,42 @@
-// Write code here.
-//
-// To see what the code looks like after macro expansion:
-//     $ cargo expand
-//
-// To run the code:
-//     $ cargo run
+// use derive_builder::Builder;
 
-fn main() {}
+// #[derive(Builder)]
+// pub struct Command {
+//     executable: String,
+//     #[builder(each = "arg")]
+//     args: Vec<String>,
+//     #[builder(each = "env")]
+//     env: Vec<String>,
+//     current_dir: Option<String>,
+// }
+
+// fn main() {
+//     let command = Command::builder()
+//         .executable("cargo".to_owned())
+//         .arg("build".to_owned())
+//         .arg("--release".to_owned())
+//         .build()
+//         .unwrap();
+
+//     assert_eq!(command.executable, "cargo");
+//     assert_eq!(command.args, vec!["build", "--release"]);
+// }
+
+use derive_builder::Builder;
+
+#[derive(Builder)]
+pub struct Command {
+    executable: String,
+    args: Vec<String>,
+    env: Vec<String>,
+    current_dir: String,
+}
+
+// struct A {
+//     a: i32,
+// }
+
+fn main() {
+
+}
+
